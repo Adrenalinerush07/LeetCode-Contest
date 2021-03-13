9de92f4a6475c0b63f98ee4d2582ed862969324d
cyberghost002 <49005095+cyberghost002@users.noreply.github.com>
2021-03-13 16:55:01 +0530

    15-03-21

---
+++ Weekly-227/d.cpp
@@ -0,0 +1,59 @@
+class Solution
+{
+public:
+    vector<int> gen_sum(vector<int> &h)
+    {
+        vector<int> t;
+        // debug(h);
+        // cout<<(1<<h.size())<<endl;
+        for (int i = 0; i < 1 << h.size(); i++)
+        {
+            // debug("ds");
+            int sum = 0;
+            for (int j = 0; j < h.size(); j++)
+            {
+                if ((1 << j) & i)
+                {
+                    sum += h[j];
+                }
+            }
+            t.push_back(sum);
+        }
+        return t;
+    }
+    int minAbsDifference(vector<int> &nums, int goal)
+    {
+        vector<int> half1, half2;
+        int i;
+        for (i = 0; i < nums.size() / 2; i++)
+        {
+            half1.push_back(nums[i]);
+        }
+        for (; i < nums.size(); i++)
+        {
+            half2.push_back(nums[i]);
+        }
+        vector<int> a = gen_sum(half1);
+        vector<int> b = gen_sum(half2);
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        // debug(a, b);
+
+        int ans = LLONG_MAX;
+        for (auto i : a)
+        {
+            auto rem = lower_bound(b.begin(), b.end(), (goal - i)) - b.begin();
+            if (rem > 0)
+            {
+                ans = min(ans, abs(goal - (i + b[rem - 1])));
+            }
+            if (rem < b.size())
+            {
+                ans = min(ans, abs(goal - (i + b[rem])));
+            }
+        }
+
+        return ans;
+    }
+};
